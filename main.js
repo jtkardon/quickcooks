d8a8ccc308860ab42ce517fc7ae83117b38f5ddd
arcash4 <arcash@wpi.edu>
2024-10-09 21:43:36 -0400

    added features

--- main.js
+++ main.js
@@ -1,7 +1,66 @@
-function main() {
-    //You have to use the callapi function like this.
-    //It's asynchronous so it's gotta be like this
-    CallAPI("chicken breast").then(data => {
-        console.log("API Call Result:", data);
-    });
+let foodList = []
+
+
+
+function addFood (event){
+    event.preventDefault();
+    console.log('YES')
+
+    const foodItem = document.querySelector('#newIngredient').value;
+    if (foodItem) {
+        foodList.push(foodItem);
+        const finalList = document.getElementById('ingredient-list');
+        const listItem = document.createElement('li');
+        listItem.textContent = foodItem;
+        finalList.appendChild(listItem);
+    }
+    updateRecipies()
+}
+
+async function updateRecipies(){
+    recipes = []
+    for (const ingredient of foodList) {
+        try {
+            const list = await CallAPI(ingredient)
+            if (list.meals) {
+                list.meals.forEach(l => {
+                    recipes.push(l)
+                    console.log(l)
+                });
+            } else {
+                console.error('No meals found for ingredient:', ingredient);
+            }
+        } catch (error) {
+            console.error('Error fetching recipes:', error);
+        }
+    }
+
+    const recipeList = document.getElementById('recipe-list');
+    recipes.forEach(r => {
+        
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+
+        const image = document.createElement('img');
+        image.src = r.strMealThumb
+        //image.alt = recipe.strMeal
+        image.style.width = '50px'
+        image.style.height = '50px'
+        image.style.objectFit = 'cover'
+
+        link.textContent = r.strMeal
+        link.href = `https://www.themealdb.com/meal/${r.idMeal}`
+        link.target = '_blank'
+        listItem.appendChild(image);
+        listItem.appendChild(link)
+        recipeList.appendChild(listItem)
+    })
+}
+
+window.onload = function() {
+    const addButton = document.querySelector('#submit')
+
+    if (addButton) {
+        addButton.onclick = (event) => addFood(event);
+    }
 }
